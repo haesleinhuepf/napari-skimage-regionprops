4ffd6b0b0392708d044fadcc9cc5a3321980f605
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-04-27 07:33:19 +0200

    added test with dask

--- napari_skimage_regionprops/_tests/test_function.py
+++ napari_skimage_regionprops/_tests/test_function.py
@@ -235,6 +235,51 @@ def test_3d(make_napari_viewer):
     print(area_measurements)
     assert np.array_equal([27, 18, 18, 12], area_measurements)
 
+
+def test_3d_dask(make_napari_viewer):
+
+    viewer = make_napari_viewer()
+    import dask.array as da
+
+    num_dw = len(viewer.window._dock_widgets)
+
+
+    import numpy as np
+
+    image = np.asarray([
+        [0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 2, 2],
+        [1, 1, 1, 0, 0, 2, 2],
+        [1, 1, 1, 0, 0, 2, 2],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 3, 3, 3, 4, 4],
+        [0, 0, 3, 3, 3, 4, 4],
+    ]).astype(float)
+
+    # make a 3D dataset
+    image = da.asarray(np.asarray([image, image, image]))
+    labels_image = da.asarray(image.astype(int))
+
+    image_layer = viewer.add_image(image)
+    labels_layer = viewer.add_labels(labels_image)
+
+    # analyze everything we can
+    from napari_skimage_regionprops import regionprops
+    regionprops(image_layer, labels_layer, viewer, True, True, True, True, True, True)
+
+    # there is now a table in the viewer
+    assert len(viewer.window._dock_widgets) == num_dw + 1
+
+    from napari_skimage_regionprops import get_table
+    table_widget = get_table(labels_layer, viewer)
+    assert table_widget is not None
+
+    area_measurements = table_widget.get_content()['area']
+    print(area_measurements)
+    assert np.array_equal([27, 18, 18, 12], area_measurements)
+
+
+
 def test_4d_3d(make_napari_viewer):
 
     viewer = make_napari_viewer()
