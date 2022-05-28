2c688690db2ffc5ee0a0f6f3b57afb8323c015da
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-05-28 21:41:56 +0200

    bump version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     packages=find_packages(),
     python_requires='>=3.6',
     install_requires=requirements,
-    version='0.5.0',
+    version='0.5.1',
     #use_scm_version=use_scm,
     setup_requires=['setuptools_scm'],
     classifiers=[
