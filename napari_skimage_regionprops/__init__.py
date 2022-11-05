660f2a9915fb6d172d4bd064d0f85e2818bd9b5d
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-11-05 21:24:09 +0100

    measure_and_visualize_points

--- napari_skimage_regionprops/__init__.py
+++ napari_skimage_regionprops/__init__.py
@@ -1,6 +1,7 @@
 from ._table import add_table, get_table, TableWidget
 from ._regionprops import regionprops, regionprops_table, regionprops_table_all_frames
 from ._parametric_images import visualize_measurement_on_labels, relabel
+from ._measure_points import measure_points
 from napari_plugin_engine import napari_hook_implementation
 from ._load_csv import load_csv
 
