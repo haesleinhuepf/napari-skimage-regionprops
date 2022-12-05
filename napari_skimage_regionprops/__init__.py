86506321a8d591b48bc9edcbcd40e4887cf10bdf
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-12-05 13:36:51 +0100

    Wrap _table import in try/except

--- napari_skimage_regionprops/__init__.py
+++ napari_skimage_regionprops/__init__.py
@@ -1,8 +1,15 @@
-from ._table import add_table, get_table, TableWidget
 from ._regionprops import regionprops, regionprops_table, regionprops_table_all_frames
 from ._parametric_images import visualize_measurement_on_labels, relabel
 from napari_plugin_engine import napari_hook_implementation
 from ._load_csv import load_csv
+
+try:
+    from ._table import add_table, get_table, TableWidget
+except ModuleNotFoundError as e:
+    import logging
+    logging.warning(e)
+    class TableWidget:
+        pass
 
 try:
     from ._version import version as __version__
