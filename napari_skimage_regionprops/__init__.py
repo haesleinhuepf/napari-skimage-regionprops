7b0855cbf36dc7617b0ecfdbd49f9779ca8161af
Robert Haase <haesleinhuepf@users.noreply.github.com>
2023-03-14 13:15:41 +0100

    bump version

--- napari_skimage_regionprops/__init__.py
+++ napari_skimage_regionprops/__init__.py
@@ -12,7 +12,7 @@ from ._load_csv import load_csv
 try:
     from ._version import version as __version__
 except ImportError:
-    __version__ = "0.9.0"
+    __version__ = "0.10.0"
 
 
 @napari_hook_implementation
