3dffb00e8f82ab207098ada63821e787b41b94af
Marcelo Zoccoler <marcelozoc@gmail.com>
2024-04-24 11:29:07 +0200

    Allow load_csv to load to any layer type, initialize surface features and properties, and match table shape in properties of points layer to number of points

--- napari_skimage_regionprops/_load_csv.py
+++ napari_skimage_regionprops/_load_csv.py
@@ -9,7 +9,7 @@ except Exception as e:
 
 
 @register_function(menu="Measurement > Load from CSV (nsr)")
-def load_csv(csv_filename:"magicgui.types.PathLike", labels_layer: "napari.layers.Labels", show_table: bool = True, viewer: "napari.Viewer" = None):
+def load_csv(csv_filename:"magicgui.types.PathLike", labels_layer: "napari.layers.Layer", show_table: bool = True, viewer: "napari.Viewer" = None):
     """Save contents of a CSV file into a given layer's properties"""
     import pandas as pd
     # load region properties from csv file
@@ -24,9 +24,18 @@ def load_csv(csv_filename:"magicgui.types.PathLike", labels_layer: "napari.layer
             {"label": np.array(range(1, (len(edited_reg_props) + 1)))}
         )
         edited_reg_props = pd.concat([label_column, edited_reg_props], axis=1)
-
+    # Add 'properties' and 'features' attributes if layer is Surface
+    if isinstance(labels_layer, napari.layers.Surface):
+        # Check if features and properties are already defined
+        if not hasattr(labels_layer, "features"):
+            labels_layer.features = {}
+        if not hasattr(labels_layer, "properties"):
+            labels_layer.properties = {}
+    elif isinstance(labels_layer, napari.layers.Points):
+        # Table shape must match number of points, so truncate if necessary
+        edited_reg_props = edited_reg_props.iloc[:labels_layer.data.shape[0],:]
     if hasattr(labels_layer, "properties"):
-        labels_layer.properties = edited_reg_props
+        labels_layer.properties = edited_reg_props.to_dict(orient="list")
     if hasattr(labels_layer, "features"):
         labels_layer.features = edited_reg_props
 
